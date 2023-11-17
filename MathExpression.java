decaa160b0e03ca1b75103d657b46ecd414e474f
Denis <denischen242@gmail.com>
2023-11-17 17:15:16 -0500

    variables

--- MathExpression.java
+++ MathExpression.java
@@ -1,11 +1,28 @@
 import com.fathzer.soft.javaluator.DoubleEvaluator;
+import com.fathzer.soft.javaluator.StaticVariableSet;
 
 public class MathExpression {
     private String stringExpression;
+    private DoubleEvaluator evaluator = new DoubleEvaluator(); // evaluator for this expression
+    final StaticVariableSet<Double> variables = new StaticVariableSet<Double>(); // stores static math variables
     private double value; // stores value of this expression
 
     public MathExpression(String stringExpression) {
         this.stringExpression = stringExpression;
+        this.value = evaluate();
+    }
+    public MathExpression(String stringExpression, String vars) {
+        this.stringExpression = stringExpression;
+        vars = vars.replace("\\s+",""); // get rid of spaces
+        String[] varList = vars.split("[=,]"); // left with an array [var1, val1, var2, val2, ... ]
+        for (int i = 0; i < varList.length - 1; i+=2) {
+            String var_i_stringValue = varList[i+1]; // corresponding value for variable i
+            if (var_i_stringValue.isEmpty()) {
+                var_i_stringValue = "0"; // default to 0
+            }
+            this.variables.set(varList[i], Double.parseDouble(var_i_stringValue));
+        }
+
         this.value = evaluate();
     }
 
@@ -13,8 +30,30 @@ public class MathExpression {
      * set this.value to that value
      * */
     private double evaluate() {
-        DoubleEvaluator evaluator = new DoubleEvaluator();
-        return evaluator.evaluate(this.stringExpression);
+        double value = 0;
+        boolean modified = false; // true iff value was modified, and the default value of 0 was overwritten
+        // if we do double value; then the compiler doesn't like that, so we gotta initialize it
+        // but we want to know if there is ever a case where the value never gets modified and is instead evaluated as its initial value
+
+        try {
+            value = evaluator.evaluate(this.stringExpression, variables);
+            modified = true;
+        } catch (IllegalArgumentException iae) {
+            String eMessage = iae.getMessage(); // error message: "x is not a number"
+
+            // take the first word in the error message (variable name)
+            String varName = eMessage.split(" ")[0];
+
+            variables.set(varName, 0.0); // set it to default value of 0
+            value = evaluate(); // call our method again
+            modified = true;
+        }
+        if (!modified) {
+            System.out.println("!--------------------------------------------------!");
+            System.out.println("!--------------- Value not modified ---------------!");
+            System.out.println("!--------------------------------------------------!");
+        }
+        return value;
     }
 
     /**
@@ -24,8 +63,8 @@ public class MathExpression {
     public double getValue() { return this.value; }
 
     public static void main(String[] args) {
-        MathExpression mex = new MathExpression("5+6-(6*7)");
-        System.out.println(mex.evaluate());
+        MathExpression mex = new MathExpression("5+6-(sin(3*x)+7*y)", "x=2,y=7");
+        System.out.println(mex.getValue());
     }
 
 }
