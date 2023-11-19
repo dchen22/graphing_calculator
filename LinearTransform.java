a6bc72e5b68513889fa550795d88a6786b3daeae
Denis <denischen242@gmail.com>
2023-11-19 12:17:21 -0500

    fixed matrix multiplication

--- LinearTransform.java
+++ LinearTransform.java
@@ -2,8 +2,8 @@ public class LinearTransform {
     public static double[] applyPointTransform(double[] point, MathMatrix matrix) {
         double[] newPoint = new double[matrix.getSize()];
         double[][] matrixArray = matrix.getArray();
-        newPoint[0] = point[0]*matrixArray[0][0] + point[1]*matrixArray[1][0];
-        newPoint[1] = point[0]*matrixArray[0][1] + point[1]*matrixArray[1][1];
+        newPoint[0] = matrixArray[0][0]*point[0] + matrixArray[0][1]*point[1];
+        newPoint[1] = matrixArray[1][0]*point[0] + matrixArray[1][1]*point[1];
         return newPoint;
     }
 
